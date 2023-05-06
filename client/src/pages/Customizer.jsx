d9d8756ae09153d9b2f7d30f8c4ff72d8f433609
lakshayysaini <lakshayysaini@gmail.com>
2023-05-06 20:15:03 +0530

    updated custom button and new files for customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+  
 const Customiser = () => {
   return (
     <div>Customiser</div>
