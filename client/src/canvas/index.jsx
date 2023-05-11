d42f51fd54d8a88d0d96814077f2c325c48edd29
lakshayysaini <lakshayysaini@gmail.com>
2023-05-11 23:53:31 +0530

    update shadows of main canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import {Canvas} from '@react-three/fiber'
-import {Environment, Center} from '@react-three/drei'
-import Shirt from './Shirt'
-import BackDrop from './BackDrop'
-import CameraRig from './CameraRig'
-  
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import BackDrop from "./BackDrop";
+import CameraRig from "./CameraRig";
+
 const CanvasModel = () => {
   return (
     <Canvas
       shadows
-    camera={{position : [0,0,0] , fov:25}}>
-      <ambientLight intensity={ 0.5 } />
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={ { preserveDrawingBuffer: true } }
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
         <BackDrop />
@@ -19,7 +22,7 @@ const CanvasModel = () => {
         </Center>
       </CameraRig>
     </Canvas>
-  ) 
-}
-//1hr10min24seconds
+  );
+};
+
 export default CanvasModel;
